fbb347ef867f912ed0db25b72f784d5f6ba8dc90
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-10 14:43:36 -0500

    Routes exercise with bonus is done

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,20 @@ Route::get('/sayhello/{name?}', function($name = 'Lassen'){
 	}
 	return 'Hello '. $name;
 });
+
+//Create a route at the path /uppercase that takes a parameter that is a word and returns a string that is the word in all caps.
+
+Route::get('/uppercase/{word?}',function($word = 'codeup'){
+	return strtoupper($word);
+});
+
+//Create a route at the path /increment that takes a parameter that is a number and returns the number plus one
+Route::get('/increment/{number?}',function($number = 1){
+	return $number + 1;
+});
+//route to add the two numbers given together
+Route::get('/add/{a?}/{b?}', function($a = 2, $b = 3){
+	return $a + $b;
+});
+
+
