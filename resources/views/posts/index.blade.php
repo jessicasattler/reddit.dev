5678f298bfe9ff587375ead84a7c8215d771dfbb
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-15 18:46:32 -0500

    Added background image and refactored css styling

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,8 @@
 @section('title','Index')
 
 @section('content')
-	<h1 style="color:Black;margin-left:30px;">Blog</h1>
+	{{-- <h1 style="color:Black;margin-left:30px;">Blog</h1> --}}
+	<h1>Blog</h1>
 	@foreach($posts as $post)
 		<div style = "background-color:#2F629B; color:white;margin-top:15px;"class="container">
 			<h3>{{$post->title}}</h3>
