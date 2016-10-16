36d83ca6328bd1012ff015d3a2b719ec7e756889
Jessica Sattler <jessicasattler28@gmail.com>
2016-10-15 21:17:46 -0500

    changed background gradient for container

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 	<h1>Individual User</h1>
-	<div style = "background-color:#2F629B; color:white;margin-top:15px;"class="container">
+	<div class="container around">
 		<h3>{{$user->name}}</h3>
 		<p>{{$user->email}}</p>
 		<hr>
